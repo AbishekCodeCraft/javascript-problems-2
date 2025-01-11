c3dd0c40d54bd2a0439980cd73c031f78c8a7184
Abishek <130296736+AbishekCodeCraft@users.noreply.github.com>
2025-01-11 19:54:09 +0530

    Add files via upload

---
+++ typeof/index.js
@@ -0,0 +1,19 @@
+function solution() {
+  var a = 235;
+  var b = "Hello";
+  var c = [1, 2, 3];
+  var d = { a: 1, b: 2 };
+  var e = true;
+  var f = false;
+
+  console.log(typeof a);
+  console.log(typeof b);
+  console.log(typeof c);
+  console.log(Array.isArray(c));
+
+  console.log(typeof d);
+  console.log(typeof e);
+  console.log(typeof f);
+}
+
+solution();
